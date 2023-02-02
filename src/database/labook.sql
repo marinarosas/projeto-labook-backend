9bf35148356035cd1bc77b6cef0b5a58501c9161
Mjfreedom <mbuzina@gmail.com>
2023-02-02 16:20:54 -0300

    :sparkles: feat: Criada a tabela posts

--- src/database/labook.sql
+++ src/database/labook.sql
@@ -15,3 +15,21 @@ INSERT INTO users (id, name, email, password, role)
 VALUES
     ("u001", "Marina", "marina@email.com", "M@arina123", "Administrador"),
     ("u002", "Alex", "alex@email.com", "@Lex123", "Usuário");
+
+CREATE TABLE posts(
+    id TEXT PRIMARY KEY UNIQUE NOT NULL,
+    creator_id TEXT NOT NULL,
+    content TEXT NOT NULL,
+    likes INTEGER DEFAULT(0) NOT NULL,
+    dislikes INTEGER DEFAULT(0) NOT NULL,
+    created_at TEXT DEFAULT(DATETIME()) NOT NULL,
+    updated_at TEXT DEFAULT(DATETIME()) NOT NULL
+);
+
+SELECT * FROM posts;
+
+INSERT INTO posts (id, creator_id, content)
+VALUES
+    ("p001", "u001", "Feliz por estar no curso!"),
+    ("p002", "u001", "Cansativo o dia!"),
+    ("p003", "u002", "Trabalhando muito!");
