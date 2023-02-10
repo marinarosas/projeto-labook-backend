b931c693276a0cf48853ef9565c824ce15e1a6dc
Mjfreedom <mbuzina@gmail.com>
2023-02-10 13:00:11 -0300

    :recycle: refactor: Passando para design patterns, dto e injeção de dependencias

--- src/dtos/PostDTO.ts
+++ src/dtos/PostDTO.ts
@@ -1,13 +1,13 @@
 import { BadRequestError } from "../errors/BadRequestError"
 import { Post } from "../models/Post"
 
-export interface CreatePostInputDTO{
+export interface CreatePostInputDTO {
     id: string,
     creatorId: string,
     content: string
 }
 
-export interface CreatePostOutputDTO{
+export interface CreatePostOutputDTO {
     message: string,
     post: {
         id: string,
@@ -15,7 +15,25 @@ export interface CreatePostOutputDTO{
         content: string,
         newLikes: number,
         newDislikes: number,
-        createdAt: string,               
+        createdAt: string,
+        updatedAt: string
+    }
+}
+
+export interface EditPostInputDTO {
+    idToEdit: string,
+    content: string
+}
+
+export interface EditPostOutputDTO {
+    message: string,
+    post: {
+        idToEdit: string,
+        creatorId: string,
+        content: string,
+        likes: number,
+        dislikes: number,
+        createdAt: string,
         updatedAt: string
     }
 }
@@ -26,25 +44,25 @@ export class PostDTO {
         id: unknown,
         creatorId: unknown,
         content: unknown
-    ): CreatePostInputDTO{
-        
+    ): CreatePostInputDTO {
+
         if (typeof id !== "string") throw new BadRequestError("'id' deve ser string")
 
         if (typeof creatorId !== "string") throw new BadRequestError("'creatorId' deve ser string")
-        
+
 
         if (typeof content !== "string") throw new BadRequestError("'content' deve ser string")
-        
+
         const dto: CreatePostInputDTO = {
-                id,
-                creatorId,
-                content
+            id,
+            creatorId,
+            content
         }
 
         return dto
     }
 
-    public createPostOutput (post: Post): CreatePostOutputDTO{
+    public createPostOutput(post: Post): CreatePostOutputDTO {
         const dto: CreatePostOutputDTO = {
             message: "Post criado com sucesso",
             post: {
@@ -53,12 +71,49 @@ export class PostDTO {
                 content: post.getContentPost(),
                 newLikes: post.getLikesPost(),
                 newDislikes: post.getDislikesPost(),
-                createdAt: post.getCreatedAtPost(),                         
+                createdAt: post.getCreatedAtPost(),
                 updatedAt: post.getUpdatedAtPost()
 
             }
         }
 
         return dto
+    }
+
+    public editPostInput(
+        idToEdit: unknown | undefined,
+        content: unknown | undefined
+    ): EditPostInputDTO {
+
+        if (typeof idToEdit !== "string") throw new BadRequestError("'id' deve ser string")
+
+
+        if (typeof content !== "string") throw new BadRequestError("'title' deve ser string")
+
+        const dto = {
+            idToEdit,
+            content
+        }
+
+        return dto
+
+    }
+
+    public editPostOutput(post: Post): EditPostOutputDTO {
+        const dto: EditPostOutputDTO = {
+            message: "Post editado com sucesso",
+            post: {
+                idToEdit: post.getIdPost(),
+                creatorId: post.getCreatorIdPost(),
+                content: post.getContentPost(),
+                likes: post.getLikesPost(),
+                dislikes: post.getDislikesPost(),
+                createdAt: post.getCreatedAtPost(),
+                updatedAt: post.getUpdatedAtPost()
+            }
+        }
+
+        return dto
+        
     }
 }
