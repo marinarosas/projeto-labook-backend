783f3065abfdad12981b7817e727d5ccb76ebbcd
Mjfreedom <mbuzina@gmail.com>
2023-02-13 14:55:30 -0300

    :recycle: refactor: Passando editPost para o arquivo DTO

--- src/router/postRouter.ts
+++ src/router/postRouter.ts
@@ -1,9 +1,15 @@
 import express from 'express'
+import { PostBusiness } from '../business/PostBusiness'
 import { PostController } from '../controller/PostController'
+import { PostsDatabase } from '../database/PostsDataBase'
 
 export const postRouter = express.Router()
 
-const postController = new PostController()
+const postController = new PostController(
+    new PostBusiness(
+        new PostsDatabase
+    )
+)
 
 postRouter.get("/", postController.getPosts)
 postRouter.post("/", postController.createPost)
