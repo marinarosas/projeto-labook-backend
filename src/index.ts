2205c7dab267db559bf22038a7a07dd2b41ec7b3
Mjfreedom <mbuzina@gmail.com>
2023-02-14 13:14:53 -0300

    :sparkles: feat: Criado o arquivo .env

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,20 @@
 import express from 'express'
 import cors from 'cors'
+import dotenv from 'dotenv'
 import { postRouter } from './router/postRouter'
 import { userRouter } from './router/userRouter'
 
 console.log('Hello world!')
+
+dotenv.config()
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 
-app.listen(3003, () => {
-    console.log("Servidor rodando na porta 3003")
+app.listen(Number(process.env.PORT), () => {
+    console.log(`Servidor rodando na porta ${Number(process.env.PORT)}`)
 })
 
 app.use("/posts", postRouter)
